0178d534fc8e23f204b9480e3d6228bde8f97151
pgdureza <pgdureza@gmail.com>
2023-01-14 12:42:58 +0800

    added button and pagination components

--- packages/twconfig/tailwind.config.js
+++ packages/twconfig/tailwind.config.js
@@ -5,7 +5,60 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#f4f3ff',
+          100: '#ebe9fe',
+          200: '#d9d6fe',
+          300: '#bdb6fc',
+          400: '#9c8cf9',
+          500: '#8062f5',
+          600: '#6a3ceb',
+          700: '#5b29d8',
+          800: '#4c22b5',
+          900: '#401e94',
+        },
+        tertiary: {
+          50: '#fbf4ff',
+          100: '#f6e8ff',
+          200: '#edd1fd',
+          300: '#e2adfa',
+          400: '#c962f5',
+          500: '#bc49ec',
+          600: '#a129d0',
+          700: '#891fac',
+          800: '#711b8d',
+          900: '#601c73',
+        },
+        secondary: {
+          50: '#fff4fd',
+          100: '#ffe8fc',
+          200: '#fdd1f7',
+          300: '#faadeb',
+          400: '#f562d7',
+          500: '#ec49ca',
+          600: '#d029ab',
+          700: '#ac1f88',
+          800: '#8d1b70',
+          900: '#731c5b',
+        },
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
+
   plugins: [],
 }
--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,73 +1,62 @@
-import { useMemo } from 'react'
+import classnames from 'classnames'
 
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Button contents
-   */
-  label: string
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void
+type Variant = 'primary' | 'secondary' | 'text'
+
+export interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant?: Variant
+  danger?: boolean
+  size?: 'small' | 'regular' | 'large'
+  wide?: boolean
+  loading?: boolean
 }
 
-const getSizeClasses = (size: string) => {
-  switch (size) {
-    case 'small': {
-      return 'px-4 py-2'
-    }
-    case 'large': {
-      return 'px-6 py-3'
-    }
-    default: {
-      return 'px-5 py-2'
-    }
-  }
-}
-
-const getModeClasses = (isPrimary: boolean) =>
-  isPrimary
-    ? 'text-white bg-pink-600 border-pink-600'
-    : 'text-slate-700 bg-transparent border-slate-700'
-
-const BASE_BUTTON_CLASSES =
-  'cursor-pointer rounded-full border-2 font-bold leading-none inline-block'
-
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  label,
-  ...props
+  wide = false,
+  variant = 'primary',
+  size = 'regular',
+  danger,
+  children,
+  className,
+  ...otherProps
 }: ButtonProps) => {
-  const computedClasses = useMemo(() => {
-    const modeClass = getModeClasses(primary)
-    const sizeClass = getSizeClasses(size)
-
-    return [modeClass, sizeClass].join(' ')
-  }, [primary, size])
-
   return (
     <button
       type="button"
-      className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
-      {...props}
+      className={classnames(
+        'transition-all duration-200 ease-in-out',
+        'hover:bg-opacity-70 hover:text-opacity-70 hover:border-opacity-70',
+        'cursor-pointer rounded-full border leading-none inline-block',
+        {
+          'px-2 py-1 text-sm': size === 'small',
+          'px-4 py-2 text-base': size === 'regular',
+          'px-8 py-4 text-lg': size === 'large',
+          'w-full': wide,
+        },
+        danger
+          ? {
+              'text-white bg-red-600 border-red-600': variant === 'primary',
+              'text-red-600 bg-transparent border-red-600':
+                variant === 'secondary',
+              'text-red-600 bg-transparent border-transparent':
+                variant === 'text',
+            }
+          : {
+              'text-white bg-primary-600 border-primary-600':
+                variant === 'primary',
+              'text-primary-600 bg-transparent border-primary-600':
+                variant === 'secondary',
+              'text-primary-600 bg-transparent border-transparent':
+                variant === 'text',
+            },
+        className
+      )}
+      {...otherProps}
     >
-      {label}
+      {children}
     </button>
   )
 }
