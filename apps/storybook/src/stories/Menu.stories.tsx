470facdb0f8e1b203320a2d32a08aae26bc83af0
Rj Villanueva <rj.villanueva89@gmail.com>
2023-01-17 18:08:52 +0800

    updated Menu component

--- apps/storybook/src/stories/Menu.stories.tsx
+++ apps/storybook/src/stories/Menu.stories.tsx
@@ -16,17 +16,86 @@ export default {
 
 const Template: ComponentStory<typeof Menu> = (args) => <Menu {...args} />
 
+const STYLE = { height: '50px', marginBottom: '15px' }
+const LINKS = [
+  { text: 'Home', url: 'javascript:void(0)' },
+  { text: 'Dashboard', url: 'javascript:void(0)' },
+  { text: 'Contact Us', url: 'javascript:void(0)' },
+]
+
 const defaultProps = {
-  items: [
-    { text: 'Home', url: 'javascript:void(0)' },
-    { text: 'Dashboard', url: 'javascript:void(0)' },
-    { text: 'Contact Us', url: 'javascript:void(0)' },
-  ],
+  items: LINKS,
 }
 
 export const Default = Template.bind({})
 Default.args = {
   ...defaultProps,
-  itemClasses: 'p-4 bg-slate-50 hover:bg-slate-150',
-  containerClasses: 'gap-2 items-center justify-end',
+  variant: 'primary',
+  size: 'regular',
+  flexJustify: 'start',
 }
+
+export const Variants: ComponentStory<typeof Menu> = () => (
+  <div>
+    <div style={STYLE}>
+      <Menu items={LINKS} variant="primary" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} variant="secondary" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} variant="text" />
+    </div>
+  </div>
+)
+
+export const Sizes: ComponentStory<typeof Menu> = () => (
+  <div>
+    <div style={STYLE}>
+      <Menu items={LINKS} size="large" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} size="regular" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} size="small" />
+    </div>
+  </div>
+)
+
+export const flexJustify: ComponentStory<typeof Menu> = () => (
+  <div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="start" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="end" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="center" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="between" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="around" />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexJustify="evenly" />
+    </div>
+  </div>
+)
+
+export const flexGap: ComponentStory<typeof Menu> = () => (
+  <div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexGap={1} />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexGap={2} />
+    </div>
+    <div style={STYLE}>
+      <Menu items={LINKS} flexGap={3} />
+    </div>
+  </div>
+)
--- packages/ui/components/Menu.tsx
+++ packages/ui/components/Menu.tsx
@@ -6,22 +6,65 @@ export interface MenuItem {
   url: string
 }
 
+export type MenuVariant = 'primary' | 'secondary' | 'text'
+
 export interface MenuProps {
   items: MenuItem[]
-  itemClasses?: string
+  variant?: MenuVariant
+  size?: 'small' | 'regular' | 'large'
+  flexJustify?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly'
+  flexGap?: number
   containerClasses?: string
 }
 
 export const Menu = ({
   items,
-  itemClasses = 'p-4 bg-slate-50 hover:bg-slate-100',
-  containerClasses = 'gap-2 items-center justify-end',
+  variant = 'primary',
+  size = 'regular',
+  flexJustify = 'start',
+  flexGap,
+  containerClasses,
 }: MenuProps) => {
+  const flexGapClass = flexGap && `gap-${flexGap}`
+
   return (
-    <div className={classnames('flex', containerClasses)}>
+    <div
+      className={classnames(
+        'flex h-full w-full items-center',
+        flexGapClass,
+        {
+          'justify-start': flexJustify === 'start',
+          'justify-end': flexJustify === 'end',
+          'justify-center': flexJustify === 'center',
+          'justify-between': flexJustify === 'between',
+          'justify-around': flexJustify === 'around',
+          'justify-evenly': flexJustify === 'evenly',
+        },
+        containerClasses
+      )}
+    >
       {items.map((item, key) => {
         return (
-          <Link key={`menu-${key}`} href={item.url} className={itemClasses}>
+          <Link
+            key={key}
+            href={item.url}
+            className={classnames(
+              'border transition-all duration-200 ease-in-out',
+              {
+                'border-purple-600 bg-purple-600 text-white hover:border-purple-600/75 hover:bg-purple-600/75 hover:text-white/75':
+                  variant === 'primary',
+                'border-purple-600 bg-transparent text-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:text-white':
+                  variant === 'secondary',
+                'border-transparent bg-transparent text-purple-600 hover:text-purple-600/75':
+                  variant === 'text',
+              },
+              {
+                'px-sm py-xs text-sm': size === 'small',
+                'px-md py-st text-base': size === 'regular',
+                'px-lg py-sm text-lg': size === 'large',
+              }
+            )}
+          >
             {item.text}
           </Link>
         )
