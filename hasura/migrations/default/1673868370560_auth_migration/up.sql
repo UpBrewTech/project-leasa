d35805b5ccc3de437c2183778e28e0a88e387e8c
Rj Villanueva <rj.villanueva89@gmail.com>
2023-02-01 16:18:58 +0800

    updated migrations; fixed users.updated_at with set timestamp trigger

--- hasura/migrations/default/1673868370560_auth_migration/up.sql
+++ hasura/migrations/default/1673868370560_auth_migration/up.sql
@@ -1,5 +1,26 @@
 SET check_function_bodies = false;
--- tables
+
+-- trigger functions
+CREATE OR REPLACE FUNCTION public.fortify_credentials() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+ NEW.identifier := LOWER(REGEXP_REPLACE(TRIM(NEW.identifier), '\s+', '_'));
+ NEW.password := crypt(NEW.password, gen_salt('bf', 8));
+ RETURN NEW;
+END
+$$;
+
+CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END
+$$;
+
+-- users
 CREATE TABLE public.users (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     name text,
@@ -8,13 +29,30 @@ CREATE TABLE public.users (
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     image text,
-    "emailVerified" timestamp with time zone
+    "emailVerified" timestamp with time zone,
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
+
+CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
+
+-- user_credentials
 CREATE TABLE public.user_credentials (
     user_id uuid NOT NULL,
     identifier text NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TRIGGER fortify_credentials
+    BEFORE INSERT OR UPDATE ON public.user_credentials
+    FOR EACH ROW
+    EXECUTE FUNCTION public.fortify_credentials();
+
+--  user_providers
 CREATE TABLE public.user_providers (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     type text NOT NULL,
@@ -30,15 +68,19 @@ CREATE TABLE public.user_providers (
     oauth_token_secret text,
     oauth_token text,
     "userId" uuid NOT NULL,
-    refresh_token_expires_in bigint
+    refresh_token_expires_in bigint,
+    PRIMARY KEY (id),
+    FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+-- user_tokens
 CREATE TABLE public.user_tokens (
     token text NOT NULL,
     identifier text NOT NULL,
     expires timestamp with time zone
 );
 
--- triggers and functions
+-- tracked functions
 CREATE OR REPLACE FUNCTION public.authenticate(identifier text, password text) RETURNS public.users
     LANGUAGE sql STABLE
     AS $$
@@ -49,29 +91,3 @@ SELECT users.*
     WHERE user_credentials.identifier = authenticate.identifier
     	AND user_credentials.password = crypt(authenticate.password, user_credentials.password)
 $$;
-CREATE OR REPLACE FUNCTION public.fortify_credentials() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
- NEW.identifier := LOWER(REGEXP_REPLACE(TRIM(NEW.identifier), '\s+', '_'));
- NEW.password := crypt(NEW.password, gen_salt('bf', 8));
- RETURN NEW;
-END
-$$;
-
--- alters
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-ALTER TABLE ONLY public.user_credentials
-    ADD CONSTRAINT user_credentials_pkey PRIMARY KEY (user_id);
-CREATE TRIGGER fortify_credentials BEFORE INSERT OR UPDATE ON public.user_credentials FOR EACH ROW EXECUTE FUNCTION public.fortify_credentials();
-ALTER TABLE ONLY public.user_credentials
-    ADD CONSTRAINT user_credentials_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY public.user_providers
-    ADD CONSTRAINT user_providers_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.user_providers
-    ADD CONSTRAINT "user_providers_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
