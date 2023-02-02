beb245e134154653219b0bbb78ba279b428a541d
Rj Villanueva <rj.villanueva89@gmail.com>
2023-02-02 09:12:01 +0800

    updated avatar stories

--- apps/storybook/src/stories/Avatar.stories.tsx
+++ apps/storybook/src/stories/Avatar.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { PropsWithChildren } from 'react'
 import { Avatar } from 'ui/components/Avatar'
 
 export default {
@@ -15,37 +14,23 @@ export default {
   },
 } as ComponentMeta<typeof Avatar>
 
-const Template: ComponentStory<typeof Avatar> = (args) => (
-  <StoryWrapper>
-    <Avatar {...args} />
-  </StoryWrapper>
-)
-
-const StoryWrapper: React.FC<PropsWithChildren> = ({ children }) => (
+export const Default: ComponentStory<typeof Avatar> = () => (
   <div
     style={{
-      height: '50px',
-      width: '50px',
-      position: 'relative',
+      display: 'flex',
+      gap: '16px',
     }}
   >
-    {children}
+    <div>
+      <Avatar src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" />
+    </div>
+    <div>
+      <Avatar />
+    </div>
   </div>
 )
 
-export const Default = Template.bind({})
-Default.args = {
-  rounded: false,
-  src: 'https://flowbite.com/docs/images/people/profile-picture-5.jpg',
-}
-
-export const Rounded = Template.bind({})
-Rounded.args = {
-  rounded: true,
-  src: 'https://flowbite.com/docs/images/people/profile-picture-5.jpg',
-}
-
-export const Sizes: ComponentStory<typeof Avatar> = () => (
+export const Rounded: ComponentStory<typeof Avatar> = () => (
   <div
     style={{
       display: 'flex',
@@ -54,24 +39,59 @@ export const Sizes: ComponentStory<typeof Avatar> = () => (
   >
     <div>
       <Avatar
+        rounded
         src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-        size="sm"
       />
     </div>
     <div>
-      <Avatar
-        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-        size="md"
-      />
-    </div>
-    <div>
-      <Avatar
-        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-        size="lg"
-      />
+      <Avatar rounded />
     </div>
   </div>
 )
 
-export const MissingImage = Template.bind({})
-MissingImage.args = {}
+export const Sizes: ComponentStory<typeof Avatar> = () => (
+  <>
+    <div
+      style={{
+        display: 'flex',
+        gap: '16px',
+        marginBottom: '20px',
+      }}
+    >
+      <div>
+        <Avatar
+          src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+          size="sm"
+        />
+      </div>
+      <div>
+        <Avatar
+          src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+          size="md"
+        />
+      </div>
+      <div>
+        <Avatar
+          src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+          size="lg"
+        />
+      </div>
+    </div>
+    <div
+      style={{
+        display: 'flex',
+        gap: '16px',
+      }}
+    >
+      <div>
+        <Avatar size="sm" />
+      </div>
+      <div>
+        <Avatar size="md" />
+      </div>
+      <div>
+        <Avatar size="lg" />
+      </div>
+    </div>
+  </>
+)
