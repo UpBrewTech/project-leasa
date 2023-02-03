b1e10505c756e0ab9423d8e668878a6c44f9a883
Rj Villanueva <rj.villanueva89@gmail.com>
2023-02-03 09:59:56 +0800

    updated Toggle component and stories

--- apps/storybook/src/stories/Toggle.stories.tsx
+++ apps/storybook/src/stories/Toggle.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { Toggle } from 'ui/components/Toggle'
+import { Toggle, ToggleProps } from 'ui/components/Toggle'
 
 export default {
   title: 'Example/Toggle',
@@ -14,13 +14,37 @@ export default {
   },
 } as ComponentMeta<typeof Toggle>
 
-export const Default: ComponentStory<typeof Toggle> = () => (
-  <label
-    style={{
-      display: 'flex',
-      gap: '16px',
-    }}
-  >
-    <Toggle /> Toggle me
-  </label>
+const STYLE = {
+  display: 'flex',
+  gap: '16px',
+}
+
+export const Default: ComponentStory<typeof Toggle> = (args: ToggleProps) => (
+  <div style={STYLE}>
+    <Toggle {...args} /> Toggle me
+  </div>
+)
+
+export const Alternative: ComponentStory<typeof Toggle> = (
+  args: ToggleProps
+) => (
+  <div style={STYLE}>
+    <Toggle alternative /> Toggle me
+  </div>
+)
+
+export const Sizes: ComponentStory<typeof Toggle> = () => (
+  <>
+    <div style={STYLE}>
+      <div style={STYLE}>
+        <Toggle size="sm" />
+      </div>
+      <div style={STYLE}>
+        <Toggle size="md" />
+      </div>
+      <div style={STYLE}>
+        <Toggle size="lg" />
+      </div>
+    </div>
+  </>
 )
