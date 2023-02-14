f546265dd19f07c510b05c111cf98596fda360db
Rj Villanueva <rj.villanueva89@gmail.com>
2023-02-14 20:08:13 +0800

    update Stepper component

--- packages/ui/components/Stepper.tsx
+++ packages/ui/components/Stepper.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames'
-import { Children, PropsWithChildren } from 'react'
+import { Children, PropsWithChildren, useCallback } from 'react'
+import { Button } from './Button'
 
 interface ProgressBarProps {
   currentStep: number
@@ -7,31 +8,62 @@ interface ProgressBarProps {
 }
 
 const ProgressBar = ({ currentStep, totalSteps }: ProgressBarProps) => {
-  const percentage = Math.floor((currentStep + 1 / totalSteps) * 100)
+  const percentage = Math.floor((currentStep / totalSteps) * 100)
   return <div>{percentage}%</div>
 }
 
-type NavProps = StepperProps
-const Nav = ({ active, dispatch, children }: NavProps) => {
+interface NavProps extends Omit<StepperProps, 'dispatch'> {
+  onClick: (step: number) => void
+}
+const Nav = ({ active, onClick, children }: NavProps) => {
   return (
     <div className="mb-sm relative">
       <ol className="flex w-full items-center justify-evenly">
         {Children.map(children, (step, key) => {
           return (
             <li>
-              <span
+              <button
+                type="button"
                 className={classnames(
                   'flex h-10 w-10 items-center justify-center rounded-full font-bold',
-                  active === key ? 'bg-purple-600 text-white' : 'bg-gray-100'
+                  key <= active ? 'bg-purple-600 text-white' : 'bg-gray-100'
                 )}
+                onClick={() => onClick(key)}
               >
                 {key + 1}
-              </span>
+              </button>
             </li>
           )
         })}
       </ol>
-      <ProgressBar currentStep={active} totalSteps={Children.count(children)} />
+      <ProgressBar
+        currentStep={Number(active + 1)}
+        totalSteps={Children.count(children)}
+      />
+    </div>
+  )
+}
+
+interface FooterButtonsProps {
+  currentStep: number
+  totalSteps: number
+  onClickNext: () => void
+  onClickPrev: () => void
+}
+const FooterButtons = ({
+  currentStep,
+  totalSteps,
+  onClickNext,
+  onClickPrev,
+}: FooterButtonsProps) => {
+  return (
+    <div className="pt-sm mt-sm flex w-full items-center justify-end gap-4 border-t">
+      {currentStep !== 1 && <Button onClick={onClickPrev}>Prev</Button>}
+      {currentStep !== totalSteps ? (
+        <Button onClick={onClickNext}>Next</Button>
+      ) : (
+        <Button>Done</Button>
+      )}
     </div>
   )
 }
@@ -51,10 +83,31 @@ export const Stepper: React.FC<StepperProps> = (props) => {
   const { active, dispatch, children } = props
   const steps = Children.toArray(children)
 
+  const handleNextClick = useCallback(() => {
+    dispatch((current) => Number(current + 1))
+  }, [dispatch])
+
+  const handlePrevClick = useCallback(() => {
+    dispatch((current) => Number(current - 1))
+  }, [dispatch])
+
+  const handleNavClick = useCallback(
+    (step: number) => {
+      dispatch(step)
+    },
+    [dispatch]
+  )
+
   return (
     <div className="h-auto w-full">
-      <Nav {...props} />
+      <Nav onClick={handleNavClick} {...props} />
       {steps[active]}
+      <FooterButtons
+        currentStep={Number(active + 1)}
+        totalSteps={Children.count(children)}
+        onClickNext={handleNextClick}
+        onClickPrev={handlePrevClick}
+      />
     </div>
   )
 }
