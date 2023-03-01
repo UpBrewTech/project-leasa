f73c4ca5b9807266aa410ced1b8f963ff5f5049f
Rj Villanueva <rj.villanueva89@gmail.com>
2023-03-01 09:51:27 +0800

    updated auth migrations

--- hasura/migrations/default/1673887777407_auth_roles_migration/up.sql
+++ hasura/migrations/default/1673887777407_auth_roles_migration/up.sql
@@ -1,16 +1,15 @@
 -- table
 CREATE TABLE public.roles (
     key text NOT NULL,
-    description text NOT NULL
+    description text NOT NULL,
+    PRIMARY KEY (key)
 );
 
 -- seeds
-INSERT INTO public.roles (key, description) VALUES ('USER', 'default user');
+INSERT INTO public.roles (key, description) VALUES
+    ('USER', 'default user');
 
 -- alters
-ALTER TABLE ONLY public.roles
-    ADD CONSTRAINT roles_pkey PRIMARY KEY (key);
-
 ALTER TABLE ONLY public.users
     ADD COLUMN role text DEFAULT 'USER'::text;
 
